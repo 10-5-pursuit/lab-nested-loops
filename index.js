113b2dfb7312cde014d17b0604a141556b2bde64
Markedia Hinds <mhinds@pursuit.org>
2024-01-16 14:45:02 -0500

    Completed Question 1

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const exampleGridCalcData = require("./data/gridCalc")
+const exampleScheduleData = require("./data/schedule")
+
 /// Problem 1: Count Zeroes
 /**
  * Counts the number of zeroes in a 2D array.
@@ -8,8 +11,18 @@
  * countZeroes([[1, 0, 3], [4, 5, 0], [7, 8, 9]])
  */
 function countZeroes(matrix) {
-  // Function implementation.
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    let arr = matrix[i]
+    for (let j = 0; j < arr.length; j++) {
+    if (arr[j] === 0) {
+      count++
+      }
+   }
+  }
+return count
 }
+countZeroes([[1, 0, 3], [4, 5, 0], [7, 8, 9]])
 
 
 /// Problem 2: Search for an Element
